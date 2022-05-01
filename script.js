94b447fcc1b60c280a4af4d77f3832e9db6dd2c2
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-01 13:17:52 -0300

    Adiciona requisito 2.

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const container = document.getElementById('meme-image-container');
-const input = document.getElementById('text-input');
+const memeImageContainer = document.getElementById('meme-image-container');
+const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const memeInsert = document.getElementById('meme-insert');
+const memeImage = document.getElementById('meme-image');
 
 const addText = () => {
-  // memeText.id = 'meme-text';
-  memeText.innerHTML = input.value;
-  // container.appendChild(memeText);
+  memeText.innerHTML = textInput.value;
 };
 // addText();
 
-input.addEventListener('input', addText);
+const addImageContainer = (event) => {
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+// Para esta solução, pesquisei: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
+};
+
+textInput.addEventListener('input', addText);
+memeInsert.addEventListener('input', addImageContainer);
