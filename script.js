f8a599e01dfed0444244fb8491615fb646c1556a
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-01 19:31:51 -0300

    Adiciona requisito 6.

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-// const memeImageContainer = document.getElementById('meme-image-container');
-const textInput = document.getElementById('text-input');
+const memeImageContainer = document.getElementById('meme-image-container');
+const textoInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const memeInsert = document.getElementById('meme-insert');
 const memeImage = document.getElementById('meme-image');
+const buttonFire = document.getElementById('fire');
+const buttonWater = document.getElementById('water');
+const buttonEarth = document.getElementById('earth');
 
 const addText = () => {
-  memeText.innerHTML = textInput.value;
+  memeText.innerHTML = textoInput.value;
 };
 // addText();
 
 const addImageContainer = (event) => {
   memeImage.src = URL.createObjectURL(event.target.files[0]);
-// Para esta solução, pesquisei: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
+// https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
 };
 
-textInput.addEventListener('input', addText);
+const fire = () => {
+  memeImageContainer.style.border = '3px dashed red';
+};
+
+const water = () => {
+  memeImageContainer.style.border = '5px double blue';
+};
+
+const earth = () => {
+  memeImageContainer.style.border = '6px groove green';
+};
+
+textoInput.addEventListener('input', addText);
 memeInsert.addEventListener('input', addImageContainer);
+buttonFire.addEventListener('click', fire);
+buttonWater.addEventListener('click', water);
+buttonEarth.addEventListener('click', earth);
