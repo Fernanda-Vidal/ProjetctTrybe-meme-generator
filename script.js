510b9eb235a30aabb7b433407b0b4b95c9f96124
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-01 20:41:33 -0300

    Adiciona requisito 7.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const memeImage = document.getElementById('meme-image');
 const buttonFire = document.getElementById('fire');
 const buttonWater = document.getElementById('water');
 const buttonEarth = document.getElementById('earth');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
 const addText = () => {
   memeText.innerHTML = textoInput.value;
@@ -29,8 +33,16 @@ const earth = () => {
   memeImageContainer.style.border = '6px groove green';
 };
 
+const addMemePre = (event) => {
+  memeImage.src = event.target.src;
+};
+
 textoInput.addEventListener('input', addText);
 memeInsert.addEventListener('input', addImageContainer);
 buttonFire.addEventListener('click', fire);
 buttonWater.addEventListener('click', water);
 buttonEarth.addEventListener('click', earth);
+meme1.addEventListener('click', addMemePre);
+meme2.addEventListener('click', addMemePre);
+meme3.addEventListener('click', addMemePre);
+meme4.addEventListener('click', addMemePre);
